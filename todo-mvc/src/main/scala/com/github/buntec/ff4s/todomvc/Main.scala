4fa1aa2bd9facfad646b14e602dff401967984b7
Christoph Bunte <christophbunte@gmail.com>
2022-05-22 22:18:44 +0200

    add custom IO runtime to todomvc

--- todo-mvc/src/main/scala/com/github/buntec/ff4s/todomvc/Main.scala
+++ todo-mvc/src/main/scala/com/github/buntec/ff4s/todomvc/Main.scala
@@ -1,8 +1,25 @@
 package ff4s.todomvc
 
 import cats.effect.{IO, IOApp}
+import cats.effect.unsafe.IORuntime
+import cats.effect.unsafe.Scheduler
+import cats.effect.unsafe.IORuntimeConfig
+import scala.scalajs.concurrent.QueueExecutionContext
 
 object Main extends IOApp.Simple {
+
+  private val microtaskEC = QueueExecutionContext.promises()
+
+  @annotation.nowarn("msg=never used")
+  private val ioRuntimeWithMicrotaskEC = IORuntime.apply(
+    microtaskEC,
+    microtaskEC,
+    Scheduler.createDefaultScheduler()._1,
+    () => (),
+    IORuntimeConfig()
+  )
+
+  // override protected def runtime: IORuntime = ioRuntimeWithMicrotaskEC
 
   override def run: IO[Unit] = new App[IO].run
 
