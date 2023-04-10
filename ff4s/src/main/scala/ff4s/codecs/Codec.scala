e622176146e618da5cf7a1cc2a731d5859675015
Christoph Bunte <christophbunte@gmail.com>
2023-04-10 19:37:08 +0200

    add copyright notice to raquo/Laminar codecs

--- ff4s/src/main/scala/ff4s/codecs/Codec.scala
+++ ff4s/src/main/scala/ff4s/codecs/Codec.scala
@@ -14,7 +14,17 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2017 Nikita Gazarov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+
 package ff4s.codecs
+
+// Verbatim from https://github.com/raquo/Laminar
 
 /** This trait represents a way to encode and decode HTML attribute or DOM
   * property values.
