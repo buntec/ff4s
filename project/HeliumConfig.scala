1643513c25b5bb52406978f14e4348864cd2872c
Christoph Bunte <christophbunte@gmail.com>
2023-05-18 21:56:05 +0200

    microsite: small colorscheme tweak

--- project/HeliumConfig.scala
+++ project/HeliumConfig.scala
@@ -29,17 +29,19 @@ object HeliumConfig {
     val `teal-600` = Color.hex("0d9488")
     val `teal-700` = Color.hex("0f766e")
     val `teal-800` = Color.hex("115e59")
-    val `gray-700` = Color.hex("374151")
     val `gray-50` = Color.hex("f9fafb")
     val `gray-100` = Color.hex("f3f4f6")
     val `gray-200` = Color.hex("e5e7eb")
     val `gray-300` = Color.hex("d1d5db")
     val `gray-400` = Color.hex("9ca3af")
     val `gray-500` = Color.hex("6b7280")
+    val `gray-600` = Color.hex("4b5563")
+    val `gray-700` = Color.hex("374151")
     val `red-600` = Color.hex("dc2626")
     val `red-400` = Color.hex("f87171")
     val `orange-600` = Color.hex("ea580c")
     val `orange-400` = Color.hex("fb923c")
+    val `yellow-600` = Color.hex("ca8a04")
 
     helium.all
       .fontResources(
@@ -100,13 +102,13 @@ object HeliumConfig {
           `gray-400`, // comments
           `gray-300`, // ???
           `fuchsia-800`, // variables
-          `gray-700` // parens, braces, brackets, commas, etc.
+          `gray-600` // parens, braces, brackets, commas, etc.
         ),
         wheel = ColorQuintet(
           `pink-500`, // string interpolation
           `sky-800`, // language keywords
           `teal-600`, // method names
-          `teal-800`, // literals: numbers, strings, etc.
+          `yellow-600`, // literals: numbers, strings, etc.
           `sky-600` // class, type, object names
         )
       )
